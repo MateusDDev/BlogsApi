057ad2b253dc4faacebda962b301ec0c25f19236
MateusDDev <ms4811978@gmail.com>
2024-01-28 16:35:49 -0300

    req 2

---
+++ src/models/User.js
@@ -0,0 +1,29 @@
+module.exports = (sequelize, DataTypes) => {
+    const User = sequelize.define(
+        'User',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                primaryKey: true,
+            },
+            displayName: {
+                type: DataTypes.STRING,
+            },
+            email: {
+                type: DataTypes.STRING,
+            },
+            password: {
+                type: DataTypes.STRING,
+            },
+            image: {
+                type: DataTypes.STRING,
+            }
+        },
+        {
+            timestamps: false,
+            underscored: true,
+        }
+    );
+
+    return User;
+}
